9c61ff28503821d79d1122c7cf2c3acdc65b2a74
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-10-12 09:25:39 +0700

    update app widget

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_fam_ui7/page/homepage.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -11,13 +12,15 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
-      title: 'TestCupertino',
-      theme: CupertinoThemeData(
-        primaryColor: CupertinoColors.systemOrange,
+    return ScreenUtilInit(
+      builder: () => const CupertinoApp(
+        title: 'Restaurant App',
+        theme: CupertinoThemeData(
+          primaryColor: CupertinoColors.systemOrange,
+        ),
+        home: HomePage(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: HomePage(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
