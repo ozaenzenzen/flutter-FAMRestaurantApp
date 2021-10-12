e5ef1c0db0a8b29a45352ebb3ec5d03d1595a9ef
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-10-13 05:46:29 +0700

    add main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fam_ui7/app_widget.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
