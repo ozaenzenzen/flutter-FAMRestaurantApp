4be34512de8f454593a07902b682487d712b7cd4
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-10-12 14:15:45 +0700

    update homepage

--- lib/page/homepage.dart
+++ lib/page/homepage.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fam_ui7/domain/list_restaurant_response.dart';
 import 'package:flutter_fam_ui7/utils/datajson.dart';
+import 'package:flutter_fam_ui7/widget/list_restaurant_item.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends StatefulWidget {
@@ -23,18 +24,16 @@ class _HomePageState extends State<HomePage> {
       //   middle: Text("Restaurant App"),
       // ),
       // ignore: avoid_unnecessary_containers
+
       child: NestedScrollView(
         headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
           return <Widget>[
-            CupertinoSliverNavigationBar(
+            const CupertinoSliverNavigationBar(
               largeTitle: Text("Restaurant App"),
-              // leading: CupertinoButton(
-              //   child: Icon(Icons.add),
-              //   onPressed: () {},
-              // ),
             ),
           ];
         },
+        // body: Container(color: Colors.teal),
         body: FutureBuilder(
           future: DataJSON().getData(),
           builder: (context, AsyncSnapshot<ListRestaurantResponse> snapshot) {
@@ -52,87 +51,105 @@ class _HomePageState extends State<HomePage> {
             return Container(
               margin: EdgeInsets.symmetric(
                 horizontal: screenUtil.setWidth(10),
-                vertical: screenUtil.setHeight(10),
+                // vertical: screenUtil.setHeight(10),
               ),
-              child: ListView.builder(
-                physics: const ScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: snapshot.data!.restaurants!.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      color: CupertinoColors.white,
-                      borderRadius: BorderRadius.circular(8.0),
-                      boxShadow: const [
-                        BoxShadow(
-                          offset: Offset(0, 3),
-                          spreadRadius: 1,
-                          blurRadius: 5,
-                          color: CupertinoColors.lightBackgroundGray,
-                        ),
-                      ],
-                    ),
-                    margin: EdgeInsets.symmetric(
-                      horizontal: screenUtil.setWidth(10),
-                      vertical: screenUtil.setHeight(10),
-                    ),
-                    child: Row(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.symmetric(
-                            horizontal: screenUtil.setWidth(10),
-                            vertical: screenUtil.setHeight(10),
-                          ),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(8),
-                            child: Image(
-                              width: screenUtil.setWidth(100),
-                              image: NetworkImage(
-                                snapshot.data!.restaurants![index].pictureId
-                                    .toString(),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Text(
-                                "${snapshot.data!.restaurants![index].name}",
-                                style: CupertinoTheme.of(context)
-                                    .textTheme
-                                    .navTitleTextStyle,
-                              ),
-                              Row(
-                                children: [
-                                  Icon(CupertinoIcons.location,
-                                      size: screenUtil.setSp(18)),
-                                  Text(
-                                    "${snapshot.data!.restaurants![index].city}",
-                                    style: CupertinoTheme.of(context)
-                                        .textTheme
-                                        .actionTextStyle,
-                                  ),
-                                ],
-                              ),
-                              SizedBox(
-                                height: screenUtil.setHeight(10),
-                              ),
-                              Text(
-                                "${snapshot.data!.restaurants![index].rating}",
-                                style: CupertinoTheme.of(context)
-                                    .textTheme
-                                    .navTitleTextStyle,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
+              // color: Colors.red,
+              child: MediaQuery.removePadding(
+                // removeTop: true,
+                context: context,
+                child: ListView.builder(
+                  padding: EdgeInsets.only(
+                    top: screenUtil.setHeight(0),
+                  ),
+                  physics: const ScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: snapshot.data!.restaurants!.length,
+                  itemBuilder: (context, index) {
+                    return RestaurantItem(
+                      id: snapshot.data!.restaurants![index].id,
+                      name: snapshot.data!.restaurants![index].name,
+                      description:
+                          snapshot.data!.restaurants![index].description,
+                      pictureId: snapshot.data!.restaurants![index].pictureId,
+                      city: snapshot.data!.restaurants![index].city,
+                      rating: snapshot.data!.restaurants![index].rating,
+                      menus: snapshot.data!.restaurants![index].menus,
+                    );
+                    // return Container(
+                    //   decoration: BoxDecoration(
+                    //     color: CupertinoColors.white,
+                    //     borderRadius: BorderRadius.circular(8.0),
+                    //     boxShadow: const [
+                    //       BoxShadow(
+                    //         offset: Offset(0, 3),
+                    //         spreadRadius: 1,
+                    //         blurRadius: 5,
+                    //         color: CupertinoColors.lightBackgroundGray,
+                    //       ),
+                    //     ],
+                    //   ),
+                    //   margin: EdgeInsets.symmetric(
+                    //     horizontal: screenUtil.setWidth(10),
+                    //     vertical: screenUtil.setHeight(10),
+                    //   ),
+                    //   child: Row(
+                    //     children: [
+                    //       Container(
+                    //         margin: EdgeInsets.symmetric(
+                    //           horizontal: screenUtil.setWidth(10),
+                    //           vertical: screenUtil.setHeight(10),
+                    //         ),
+                    //         child: ClipRRect(
+                    //           borderRadius: BorderRadius.circular(8),
+                    //           child: Image(
+                    //             width: screenUtil.setWidth(100),
+                    //             image: NetworkImage(
+                    //               snapshot.data!.restaurants![index].pictureId
+                    //                   .toString(),
+                    //             ),
+                    //           ),
+                    //         ),
+                    //       ),
+                    //       Expanded(
+                    //         child: Column(
+                    //           crossAxisAlignment: CrossAxisAlignment.start,
+                    //           mainAxisAlignment: MainAxisAlignment.end,
+                    //           children: [
+                    //             Text(
+                    //               "${snapshot.data!.restaurants![index].name}",
+                    //               style: CupertinoTheme.of(context)
+                    //                   .textTheme
+                    //                   .navTitleTextStyle,
+                    //             ),
+                    //             Row(
+                    //               children: [
+                    //                 Icon(CupertinoIcons.location,
+                    //                     size: screenUtil.setSp(18)),
+                    //                 Text(
+                    //                   "${snapshot.data!.restaurants![index].city}",
+                    //                   style: CupertinoTheme.of(context)
+                    //                       .textTheme
+                    //                       .actionTextStyle,
+                    //                 ),
+                    //               ],
+                    //             ),
+                    //             SizedBox(
+                    //               height: screenUtil.setHeight(10),
+                    //             ),
+                    //             Text(
+                    //               "${snapshot.data!.restaurants![index].rating}",
+                    //               style: CupertinoTheme.of(context)
+                    //                   .textTheme
+                    //                   .navTitleTextStyle,
+                    //             ),
+                    //           ],
+                    //         ),
+                    //       ),
+                    //     ],
+                    //   ),
+                    // );
+                  },
+                ),
               ),
             );
           },
