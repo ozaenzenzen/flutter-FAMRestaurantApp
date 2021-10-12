5807a7a0108ebad5789e118e9973d5ada21a9a34
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-10-12 14:15:31 +0700

    update appwidget: add get

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_fam_ui7/page/homepage.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -13,13 +15,17 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      builder: () => const CupertinoApp(
+      // builder: () => const MaterialApp(
+      // builder: () => const CupertinoApp(
+      builder: () => const GetMaterialApp(
+      // builder: () => const GetCupertinoApp(
         title: 'Restaurant App',
-        theme: CupertinoThemeData(
-          primaryColor: CupertinoColors.systemOrange,
-        ),
+        // theme: CupertinoThemeData(
+        //   primaryColor: CupertinoColors.systemOrange,
+        // ),
         home: HomePage(),
         debugShowCheckedModeBanner: false,
+        defaultTransition: Transition.cupertino,
       ),
     );
   }
