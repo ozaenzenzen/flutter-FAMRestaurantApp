5382fae0da1caf3b4d9b7dadc66e37ca6fbc32f3
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-10-13 05:46:37 +0700

    update app widget

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_fam_ui7/application/detail_restaurant/bloc/detail_restaurant_bloc.dart';
+import 'package:flutter_fam_ui7/application/list_restaurant/bloc/list_restaurant_bloc.dart';
+import 'package:flutter_fam_ui7/application/list_restaurant/cubit/list_restaurant_cubit.dart';
 import 'package:flutter_fam_ui7/page/homepage.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:get/get_navigation/src/routes/transitions_type.dart'
+    as transition;
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -14,12 +20,25 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      builder: () => const GetMaterialApp(
-        title: 'Restaurant App',
-        home: HomePage(),
-        debugShowCheckedModeBanner: false,
-        defaultTransition: Transition.cupertino,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ListRestaurantCubit>(
+          create: (context) => ListRestaurantCubit(),
+        ),
+        // BlocProvider(
+        //   create: (context) => ListRestaurantBloc(),
+        // ),
+        BlocProvider(
+          create: (context) => DetailRestaurantBloc(),
+        ),
+      ],
+      child: ScreenUtilInit(
+        builder: () => const GetMaterialApp(
+          title: 'Restaurant App',
+          home: HomePage(),
+          debugShowCheckedModeBanner: false,
+          defaultTransition: transition.Transition.cupertino,
+        ),
       ),
     );
   }
