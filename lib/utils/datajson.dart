27b2498e8bc4a48b24c787ade830323c81d35b8d
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-10-12 09:25:49 +0700

    add data json service

---
+++ lib/utils/datajson.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_fam_ui7/domain/list_restaurant_response.dart';
+import 'package:flutter_fam_ui7/utils/app_asset.dart';
+
+class DataJSON {
+  Future<ListRestaurantResponse> getData() async{
+    
+    var response = await rootBundle.loadString(AppAsset.dataJson);
+    var jsonObject = json.decode(response);
+    ListRestaurantResponse listRestaurantResponse = ListRestaurantResponse.fromJson(jsonObject);
+    return listRestaurantResponse;
+
+    // return rootBundle.loadString('asset/json/local_restaurant.json');
+  }
+}
