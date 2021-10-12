95e1de3cc0b20d1b1597041c5f39c5a4ee314d00
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-10-13 05:46:53 +0700

    update urls

---
+++ lib/urls/urls.dart
@@ -0,0 +1,18 @@
+class Urls {
+  static const String BASE_API = "https://restaurant-api.dicoding.dev";
+
+  static const String LIST_RESTAURANT = BASE_API + "/list";
+  static const String DETAIL_RESTAURANT = BASE_API + "/detail";
+
+  //BASE_API + "/search?q=query";
+  static const String SEARCH_RESTAURANT = BASE_API + "/search?q=";
+
+  static const String REVIEW_RESTAURANT = BASE_API + "/review";
+
+  static const String IMAGE_SMALL =
+      "https://restaurant-api.dicoding.dev/images/small";
+  static const String IMAGE_MEDIUM =
+      "https://restaurant-api.dicoding.dev/images/medium";
+  static const String IMAGE_LARGE =
+      "https://restaurant-api.dicoding.dev/images/large";
+}
