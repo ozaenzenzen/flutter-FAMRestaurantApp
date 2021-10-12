754ce50fba7162c6f342af32121945291f85dd56
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-10-13 05:48:30 +0700

    add list restaurant cubit

---
+++ lib/application/list_restaurant/cubit/list_restaurant_state.dart
@@ -0,0 +1,24 @@
+part of 'list_restaurant_cubit.dart';
+
+abstract class ListRestaurantState {}
+
+class ListRestaurantInitial extends ListRestaurantState {}
+
+class ListRestaurantLoading extends ListRestaurantState {}
+
+class ListRestaurantSuccess extends ListRestaurantState {}
+
+class ListRestaurantError extends ListRestaurantState {
+  final String errorMessage;
+  ListRestaurantError(this.errorMessage);
+}
+
+class ListRestaurantGetSuccess extends ListRestaurantState {
+  final ListRestaurantResponse listRestaurantResponse;
+  ListRestaurantGetSuccess(this.listRestaurantResponse);
+}
+
+class ListRestaurantSearchSuccess extends ListRestaurantState {
+  final SearchRestaurantResponse searchRestaurantResponse;
+  ListRestaurantSearchSuccess(this.searchRestaurantResponse);
+}
